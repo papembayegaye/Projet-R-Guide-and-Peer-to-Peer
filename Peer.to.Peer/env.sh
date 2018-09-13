d194ec9bcd98f3e5afd6b785faf86c28012aa37b
kanekhalide <yayakane1996@gmail.com>
2018-09-13 09:09:49 +0000

    les librairi

--- Peer.to.Peer/env.sh
+++
@@ -1,5 +0,0 @@
-export JAVA_HOME=/usr/bin/java
-export CLASSPATH=$JAVA_HOME/lib:.:..
-export JXTA_HOME=/mci/ei0912/lafontai/workspace/jxta/lib
-export PATH=${JAVA_HOME}/bin:${PATH}:.
-export CLASSPATH=${CLASSPATH}:${JXTA_HOME}/jxta.jar:${JXTA_HOME}/log4j.jar:${JXTA_HOME}/bcprov-jdk14.jar
